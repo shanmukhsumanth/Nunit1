3065caacc83f3817b83b3db9d4e8694877a38465
shanmukh sumanth <shanmukhsumanthsumanth111@gmail.com>
2021-07-15 15:42:40 +0530

    first commit

---
+++ UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace unit_testing_using_nunit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
